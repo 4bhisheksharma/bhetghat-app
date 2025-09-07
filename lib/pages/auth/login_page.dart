c6c25235c4ba6c6bbdf95eec3f2690efab61dea1
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-09-07 19:51:09 +0545

    add: implement email and password fields in LoginPage/ created custom component for text field

--- lib/pages/auth/login_page.dart
+++ lib/pages/auth/login_page.dart
@@ -1,39 +1,73 @@
+import 'package:bhetghat/components/my_text_field.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  //text controller
+  final TextEditingController emailcontroller = TextEditingController();
+  final TextEditingController passwordcontroller = TextEditingController();
+
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
       body: Center(
-        child: Column(
-          //logo image
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/bhetghat-logo.png',
-              height: 200,
-              width: 200,
-              color: Theme.of(context).colorScheme.inversePrimary,
-            ),
+        child: Padding(
+          padding: const EdgeInsets.all(25.0),
+          child: Column(
+            //logo image
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'assets/images/bhetghat-logo.png',
+                height: 200,
+                width: 200,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
 
-            //
-            Text(
-              'B H E T G H A T',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.w200),
-            ),
+              //
+              Text(
+                'B H E T G H A T',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.w200),
+              ),
 
-            // text fields for email and password
-            
+              const SizedBox(height: 25),
+              // text fields for email and password
+              MyTextField(
+                hintText: "Email",
+                obscureText: false,
+                controller: emailcontroller,
+              ),
+              const SizedBox(height: 15),
+              MyTextField(
+                hintText: "Password",
+                obscureText: true,
+                controller: passwordcontroller,
+              ),
 
-            // forget password
+              const SizedBox(height: 15),
 
-            //sign in button
+              // forget password
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      color: Theme.of(context).colorScheme.secondary,
+                    ),
+                  ),
+                ],
+              ),
 
-            //dont have an account? sign up
-          ],
+              SizedBox(height: 25),
+
+              //sign in button
+
+              //dont have an account? sign up
+            ],
+          ),
         ),
       ),
     );
