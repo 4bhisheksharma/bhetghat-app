ce0feb599976fc775ccf5ebf1d11c116300dd13f
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-09-08 19:32:05 +0545

    add: implement MyDrawer component for navigation; update HomePage to include drawer and modify AppBar background color

---
+++ lib/components/my_drawer.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatelessWidget {
+  const MyDrawer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: [
+              //header
+              DrawerHeader(
+                child: Image(
+                  image: AssetImage('assets/images/bhetghat-logo.png'),
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+
+              SizedBox(height: 10),
+
+              //home
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.home,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                  title: const Text('H O M E'),
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              ),
+
+              //profile
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.person,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                  title: const Text('P R O F I L E'),
+                  onTap: () {},
+                ),
+              ),
+
+              //users
+              Padding(
+                padding: const EdgeInsets.only(left: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.group,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                  title: const Text('U S E R S'),
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+
+          //logout
+          Padding(
+            padding: const EdgeInsets.only(left: 25.0, bottom: 25.0),
+            child: ListTile(
+              leading: Icon(
+                Icons.logout,
+                color: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              title: const Text('L O G O U T'),
+              onTap: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/pages/content/home_page.dart
+++ lib/pages/content/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:bhetghat/components/my_drawer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -14,13 +15,14 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Home Page"),
-        backgroundColor: Theme.of(context).colorScheme.primary,
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         actions: [
           IconButton(onPressed: logout, icon: const Icon(Icons.logout)),
         ],
       ),
 
       body: const Center(child: Text('Home Page')),
+      drawer: MyDrawer(),
     );
   }
 }
