a0ba2ab9977cef95af489b187cff2c92ab0edab6
Abhishek Sharma <sharmaabhi10101010@gmail.com>
2025-09-09 12:12:10 +0545

    feat: implement user details fetching in ProfilePage; update UI to display user email and username

--- lib/pages/content/profile_page.dart
+++ lib/pages/content/profile_page.dart
@@ -1,7 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({super.key});
+  ProfilePage({super.key});
+
+  //current logged in user
+  User? currentUser = FirebaseAuth.instance.currentUser;
+
+  //future to fetch user details
+  Future<DocumentSnapshot<Map<String, dynamic>>> getUserDetails() async {
+    return await FirebaseFirestore.instance
+        .collection("Users")
+        .doc(currentUser!.email)
+        .get();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +23,32 @@ class ProfilePage extends StatelessWidget {
         title: const Text("Profile"),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      body: const Center(child: Text('Profile Page')),
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      body: FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+        future: getUserDetails(),
+        builder: (context, snapshot) {
+          //loaging circle
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+          //show error
+          else if (snapshot.hasError) {
+            return Text("Error: ${snapshot.error}");
+          }
+          //data receive here
+          else if (snapshot.hasData) {
+            Map<String, dynamic>? user = snapshot.data!.data();
+
+            return Center(
+              child: Column(
+                children: [Text(user!['email']), Text(user['username'])],
+              ),
+            );
+          } else {
+            return Text("Data not Available");
+          }
+        },
+      ),
     );
   }
 }
